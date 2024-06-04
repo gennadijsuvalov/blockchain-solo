b646860d1ed16c945b191cc5a7baf7a56b6c3470
Gennadij <gennadijsuvalov307@gmail.com>
2024-06-04 08:52:29 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,77 @@
+const crypto = require('crypto');
+
+class Block {
+    constructor(index, timestamp, data, previousHash = '') {
+        this.index = index;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.previousHash = previousHash;
+        this.hash = this.calculateHash();
+        this.nonce = 0;
+    }
+
+    calculateHash() {
+        return crypto
+            .createHash('sha256')
+            .update(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce)
+            .digest('hex');
+    }
+
+    mineBlock(difficulty) {
+        while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join('0')) {
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+
+        console.log(`Block mined: ${this.hash}`);
+    }
+}
+
+class Blockchain {
+    constructor() {
+        this.chain = [this.createGenesisBlock()];
+        this.difficulty = 2;
+    }
+
+    createGenesisBlock() {
+        return new Block(0, '01/01/2021', 'Genesis Block', '0');
+    }
+
+    getLatestBlock() {
+        return this.chain[this.chain.length - 1];
+    }
+
+    addBlock(newBlock) {
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.mineBlock(this.difficulty);
+        this.chain.push(newBlock);
+    }
+
+    isChainValid() {
+        for (let i = 1; i < this.chain.length; i++) {
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i - 1];
+
+            if (currentBlock.hash !== currentBlock.calculateHash()) {
+                return false;
+            }
+
+            if (currentBlock.previousHash !== previousBlock.hash) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+// Example usage
+const myBlockchain = new Blockchain();
+console.log('Mining block 1...');
+myBlockchain.addBlock(new Block(1, '02/01/2021', { amount: 4 }));
+
+console.log('Mining block 2...');
+myBlockchain.addBlock(new Block(2, '03/01/2021', { amount: 10 }));
+
+console.log(JSON.stringify(myBlockchain, null, 2));
+console.log('Is blockchain valid?', myBlockchain.isChainValid());
